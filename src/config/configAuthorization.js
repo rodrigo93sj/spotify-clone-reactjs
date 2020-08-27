5f545964f72627689aac342d174dd016616af68b
Rodrigo <rodrigoengj@gmail.com>
2020-08-27 14:38:38 -0300

    Update api - Alterando a url do redirectUri

--- src/config/configAuthorization.js
+++ src/config/configAuthorization.js
@@ -1,5 +1,5 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "14ad9611605f43918167e0cd7d9495e7";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri = "https://spotify-clone-reactjs.herokuapp.com";
 export const scopes = ["user-top-read"];
--- src/services/api.js
+++ src/services/api.js
@@ -7,8 +7,6 @@ if (_token) {
   localStorage.setItem("token", _token);
 }
 
-console.log(process.env.REACT_APP_API_URL)
-
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
