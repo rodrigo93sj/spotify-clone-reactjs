acd73b41670991bc1114211867128b636c668152
Rodrigo <rodrigoengj@gmail.com>
2020-08-27 22:49:09 -0300

    Update variáveis de ambiente

--- src/config/configAuthorization.js
+++ src/config/configAuthorization.js
@@ -1,5 +1,5 @@
-export const authEndpoint = process.env.REACT_APP_AUTH_ENDPOINT || "https://accounts.spotify.com/authorize";
+export const authEndpoint = process.env.REACT_APP_AUTH_ENDPOINT;
 
 export const clientId = "14ad9611605f43918167e0cd7d9495e7";
-export const redirectUri = process.env.REACT_APP_REDIRECT_URI || "http://localhost:3000/";
+export const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 export const scopes = ["user-top-read"];
