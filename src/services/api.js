74d35737d945c0f818aeaf32413afdaaa063127f
Rodrigo <rodrigoengj@gmail.com>
2020-08-27 22:36:07 -0300

    Add variáveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,7 @@ if (_token) {
 }
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://api.spotify.com/v1",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
